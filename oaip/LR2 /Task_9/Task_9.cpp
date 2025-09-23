477461ae35bb9393e544b35721bca55faf75c14d
Надежда Седельник <nadezdasedelnik@MacBook-Air-Nadezda.local>
2025-09-23 20:03:02 +0300

    Task9

---
+++ oaip/LR2 /Task_9/Task_9.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+double f(double x, int choise) {
+    switch(choise) {
+        case 1:
+        cout << "Выбрана функция f(x) = 2x при x = " << x << endl;
+        return 2 * x;
+        case 2:
+        cout << "Выбрана функция f(x) = x^3 при x = " << x << endl;
+        return x * x * x;
+        case 3:
+        cout << "Выбрана функция f(x) = x / 3 при x = " << x << endl;
+        return x / 3.0;
+        default :
+        cout << "Ошибка: неверный выбор функции. " << endl;
+        return 0.0;
+    }
+}
+int main () {
+    double z, d;
+    int choise;
+    cout << "Введите значение z: ";
+    cin >> z;
+    cout << "Введите значение d: ";
+    cin >> d;
+    cout << "Выберите функцию: \n1 - f(x) = 2x\n2 - f(x) = x^3\n3 - f(x) = x / 3\nВаш выбор: ";
+    cin >> choise;
+
+    double x;
+    if (z < 0) {
+        x = z * z - z;
+        cout << "Условие: z < 0 значит x = z^2 - z = " << x << endl;
+    } else {
+        x = z * z * z;
+        cout << "Условие: z >= 0 значит x = z^3 = " << x << endl;
+    }
+    double phi = f(x, choise);
+    double theta = phi + d * d + x * x;
+    cout << "Аргумент синуса θ = f(x) + d^2 + x^2 =  " << theta << endl;
+    cout << "Результат: y = sin³(" << theta << ") - вычисляется вручную или с калькулятором." << endl;
+    return 0;
+}
+
